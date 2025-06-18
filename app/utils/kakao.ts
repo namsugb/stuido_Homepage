d5fc52a5f191f036b0dbe064d4131abfb740a23c
namsugb <namsugb@naver.com>
2025-06-19 00:23:04 +0900

    num

--- app/utils/kakao.ts
+++ app/utils/kakao.ts
@@ -28,7 +28,7 @@ export async function sendKakaoNotification(data: KakaoNotificationData) {
             messages: [
                 {
                     no: "1",
-                    tel_num: "01089300045",
+                    tel_num: "01039412259",
                     use_sms: "0",
                     sms_content: "아침햇살 스튜디오 예약 문의",
                     msg_content: `아침햇살 스튜디오님\n새로운 예약문의가 있습니다.\n\n-고객정보\n이름 : ${data.name}\n연락처 : ${data.phone}\n촬영유형 : ${data.shootingType}`,
