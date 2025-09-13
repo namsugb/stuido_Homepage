f1ea8c5bd02da5d59b492c64c4f7b1cb01c03cb2
namsugb <namsugb99@gmail.com>
2025-09-13 23:22:58 +0900

    a

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -3,6 +3,7 @@ import { eventsMetadata } from '@/app/metadata'
 import EventsClient from '@/components/features/Events/EventsClient'
 
 export const metadata: Metadata = eventsMetadata
+const dum = "dum"
 
 export default function EventsPage() {
     return <EventsClient />
